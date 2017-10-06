4d29456f56f7209391ad4d94638a25f3ca06f1b6
Craig Ferris <cmferris97@yahoo.com>
2017-10-06 18:58:10 -0400

    Added keyboard listener
    Added test code to test the keyboard listener

--- script.js
+++ script.js
@@ -1,10 +1,21 @@
 var canvas = document.getElementById("gfx");
 var context = canvas.getContext("2d");
 
+document.addEventListener("keydown", keyDownHandler,false);
+document.addEventListener("keyup",keyUpHandler,false);
+
+/* keyboard keys */
+
+var wKey = false;
+var aKey = false;
+var sKey = false;
+var dKey = false;
+
 /* Image variables */
-var logo = new Image;
+var logo = new Image();
 
 var testMoveX = 0;
+var testMoveY = 0;
 
 Main();
 
@@ -20,14 +31,30 @@ function drawImage(image, x, y, width, height)
 
 function update()
 {
-	testMoveX++;
+	//testMoveX++;
+	if(wKey == true)
+	{
+		testMoveY--;
+	}
+	if(sKey == true)
+	{
+		testMoveY++;
+	}
+	if(aKey == true)
+	{
+		testMoveX--;
+	}
+	if(dKey == true)
+	{
+		testMoveX++;
+	}
 }
 
 function render()
 {
 	context.rect(20,20,150,100);
 	context.stroke();
-	drawImage(logo,testMoveX,80,128,128);
+	drawImage(logo,testMoveX,testMoveY,128,128);
 }
 
 function Main()
@@ -45,3 +72,46 @@ function Main()
 	console.log("Test");
 }
 
+function keyDownHandler(event)
+{
+	var key = String.fromCharCode(event.keyCode);
+
+	if(key == "W")
+	{
+		wKey = true;
+	}
+	else if(key == "A")
+	{
+		aKey = true;
+	}
+	else if(key == "S")
+	{
+		sKey = true;
+	}
+	else if(key == "D")
+	{
+		dKey = true;
+	}
+}
+
+function keyUpHandler(event)
+{
+	var key = String.fromCharCode(event.keyCode);
+
+	if(key == "W")
+	{
+		wKey = false;
+	}
+	else if(key == "A")
+	{
+		aKey = false;
+	}
+	else if(key == "S")
+	{
+		sKey = false;
+	}
+	else if(key == "D")
+	{
+		dKey = false;
+	}
+}
