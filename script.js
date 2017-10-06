5e779245dd79d7365ad0fea3584311e5d3e5c7c8
Craig Ferris <cmferris97@yahoo.com>
2017-10-06 16:02:05 -0400

    added game loop

--- script.js
+++ script.js
@@ -1,12 +1,44 @@
 var canvas = document.getElementById("gfx");
 var context = canvas.getContext("2d");
 
+/* Image variables */
+var logo = new Image;
+
 Main();
 
-function Main()
+function initGame()
+{
+	logo.src = "./logo.png";
+}
+
+function drawImage(image, x, y, width, height)
+{
+	context.drawImage(image,x,y,width,height);
+}
+
+function update()
+{
+
+}
+
+function render()
 {
 	context.rect(20,20,150,100);
 	context.stroke();
+	drawImage(logo,180,80,128,128);
+}
 
+function Main()
+{
+	console.log("Running...");
+	initGame();
+
+	var mainloop = function()
+	{
+		update();
+		render();
+	};
+	setInterval(mainloop,60);
 	console.log("Test");
 }
+
